6877c902e9a1d0fef093e3ba7c90f6a5878d85fc
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-11-08 21:00:06 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Web Scrapping de Spotify
+
+#### Este scrapping nos servirá para ir monitoreando los distintos ascensos y descensos de la popularidad musical a nivel mundial
+
+#### Requisitos: puppeteer
---
+++ src/PageExplorer.js
@@ -0,0 +1,14 @@
+const URL = "https://open.spotify.com/playlist/37i9dQZEVXbLiRSasKsNU9"
+
+
+const evaluatePage = () => {
+    const mainDiv = document.getElementsByClassName("iDlSBR5JgCntHwvGPaQk")[1]
+
+    for (let song of mainDiv.children) {
+        
+    }
+
+
+}
+
+// module.exports = evaluatePage
