bb31b9f45c394f48b3af33e1e15a496fb7de9a86
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-11-08 21:26:19 -0300

    ADD PageEvaluate

--- src/PageExplorer.js
+++ src/PageExplorer.js
@@ -2,13 +2,26 @@ const URL = "https://open.spotify.com/playlist/37i9dQZEVXbLiRSasKsNU9"
 
 
 const evaluatePage = () => {
-    const mainDiv = document.getElementsByClassName("iDlSBR5JgCntHwvGPaQk")[1]
+    const mainDiv = document.getElementsByClassName("iDlSBR5JgCntHwvGPaQk")[0].children[1].children
+    const songsData = []
+    let idSong = 1
 
-    for (let song of mainDiv.children) {
+    for (let song of mainDiv) {
+        const data = song.children[0].children[1].children[1]
+        const allArtists = []
+
+        for (let i = 0; i < data.lastElementChild.children.length; i++) {
+            allArtists.push(data.lastElementChild.children[i].textContent)
+        }
+
+        songsData.push({
+            id: idSong++,
+            nameSong: data.firstElementChild.textContent,
+            artist: allArtists.toString().replace(",", ", ")
+        })
         
     }
-
-
+    return songsData
 }
 
 // module.exports = evaluatePage
